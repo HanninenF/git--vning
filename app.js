32b940f7b0744a1db94d1696ebbcc06ed079dfba
unknown <hanninenfredrik@gmail.com>
2024-09-24 23:40:51 +0200

    update basic structure 1.2

---
+++ app.js
@@ -0,0 +1,9 @@
+//initiera variabel för att lagra element som refererar till form
+const form = document.querySelector(".submit-form");
+
+console.log(`form: ${form}`);
+
+//
+console.log(form);
+const name = form.querySelector(".userName").value;
+console.log(`name: ${name}`);
